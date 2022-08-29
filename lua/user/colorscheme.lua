8bf69b708a6cc635f6999b894bc44c82a907f5e6
kamalkech <maroc.develop@gmail.com>
2022-08-29 11:43:18 +0100

    onedark

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,11 +1,11 @@
 vim.g.tokyonight_transparent = true
 vim.g.tokyonight_transparent_sidebar = true
 
-vim.cmd [[
+vim.cmd([[
 try
   colorscheme tokyonight
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
-]]
+]])
