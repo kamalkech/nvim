84460fc60552b40f2fa63507382382e32e4e4dc7
kamalkech <maroc.develop@gmail.com>
2022-08-31 21:11:27 +0100

    .

---
+++ lua/user/material.lua
@@ -0,0 +1,13 @@
+-- -- vim.cmd 'colorscheme material'
+vim.g.material_style = "deep ocean"
+--
+-- vim.cmd([[
+-- try
+--   colorscheme material
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--   colorscheme default
+--   set background=dark
+-- endtry
+-- ]])
+vim.cmd 'colorscheme material'
+
