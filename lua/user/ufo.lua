4fd81cec5bb46dd0747b20ce0bd48c2c9702e108
kamalkech <maroc.develop@gmail.com>
2022-09-06 13:25:51 +0100

    .

--- lua/user/ufo.lua
+++ lua/user/ufo.lua
@@ -2,4 +2,8 @@ vim.wo.foldcolumn = "1"
 vim.wo.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.wo.foldenable = true
 
-require("ufo").setup()
+require("ufo").setup({
+	provider_selector = function(bufnr, filetype, buftype)
+		return { "treesitter", "indent" }
+	end,
+})
