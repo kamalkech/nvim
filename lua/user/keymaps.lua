e88200e5646754f4e9e763262a22bee37ed5d86a
kamalkech <maroc.develop@gmail.com>
2022-09-15 22:42:10 +0100

    .

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -31,6 +31,8 @@ keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>", opts)
 keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<tab>", ":bnext<CR>", opts)
+keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
